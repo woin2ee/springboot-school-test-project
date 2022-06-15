2f73968cae58fb593662d2a7cdccf6521708b118
Jaewon <woin2ee@gmail.com>
2022-06-15 16:38:36 +0900

    Init Commit

---
+++ src/main/java/iducs/springboot/yjwboard/Board202012069Application.java
@@ -0,0 +1,13 @@
+package iducs.springboot.yjwboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Board202012069Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Board202012069Application.class, args);
+    }
+
+}
---
+++ src/test/java/iducs/springboot/yjwboard/Board202012069ApplicationTests.java
@@ -0,0 +1,13 @@
+package iducs.springboot.yjwboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Board202012069ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
